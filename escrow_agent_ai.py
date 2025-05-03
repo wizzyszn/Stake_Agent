92ae6d612f5cb86c33ccffdd7257040ab82d3042
wizzyszn <wisdomolubayoadedoyin@yahoo.com>
2025-05-03 21:59:52 +0100

    initialized Agent

---
+++ escrow_agent_ai.py
@@ -0,0 +1,10 @@
+import base64
+import os
+import ast
+
+"""
+Registers on Almanac so that other agents (player or challenger) can discover it
+Waits for two Escrow Requests
+Fetched BTC prices from Binance
+Transfers the correct portion of SOl to the winner
+"""
