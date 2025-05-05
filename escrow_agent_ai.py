374118637a2482623fdae0057e8e0d72b5b799dd
wizzyszn <wisdomolubayoadedoyin@yahoo.com>
2025-05-05 23:44:03 +0100

    set storage for bet collection

--- escrow_agent_ai.py
+++ escrow_agent_ai.py
@@ -4,6 +4,8 @@ import ast
 from uagents import Agent, Context, Model
 from solders.keypair import Keypair
 import time
+from dataclasses import dataclass
+from typing import Union, Dict, TypeAlias
 
 """
 Registers on Almanac so that other agents (player or challenger) can discover it
@@ -39,10 +41,23 @@ def start_up_func(ctx: Context):
     ctx.logger.info("Escrow Agent initialized, ready for bids.")
     ctx.logger.info(f"Escrow agent address {agent.address}")
     ctx.storage.set("bids_count", 0)
+    ctx.storage.set("storage", {})
 
 
 # Handle messaging
 # Recieving bets
 @agent.on_message(model=EscrowRequest, replies=EscrowResponse)
-async def escrow_request_handler(ctx: Context, sender: str, msg: EscrowRequest):
+async def escrow_request_handler(ctx: Context, comparison: str, msg: EscrowRequest):
+
+    storage: Dict[str, Union[EscrowRequest, dict]] = {}
     current_count = agent.storage.get("bids_count") or 0
+    if current_count == 0:
+        storage[msg.public_key] = EscrowRequest(
+            price=msg.price, amount=msg.amount, public_key=msg.public_key
+        )
+        ctx.storage.set("storage", storage)
+    elif current_count == 1:
+        storage[msg.public_key] = EscrowRequest(
+            price=msg.price, amount=msg.amount, public_key=msg.public_key
+        )
+        
